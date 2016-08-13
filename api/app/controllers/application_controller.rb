ad1ca14559e050bbd8e0dd9c861417b86ee890d1
macroscopic <mac.roche@gmail.com>
2016-08-12 21:14:56 -0700

    completed inital phase of OAuth validations

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -57,10 +57,12 @@ class ApplicationController < ActionController::Base
     pp response
   end
 
-  private
 
   def current_user
-    @current_user ||= User.find(session[:user_id] if session[:user_id])
+
+    if session[:user_id]
+     @current_user ||=  User.find(session[:user_id])
+    end
   end
 
   helper_method :current_user
