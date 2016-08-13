a17e803de02b216df11a43bf80900baff8a426dd
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-13 14:48:11 -0700

    add associations for ingredients, recipes, and users

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-  has_many :user_ingredients, foreign_key: :ingredient_id
-  has_many :recipe_ingredients, foreign_key: :ingredient_id
-  has many :users, through: :user_ingredients, foreign_key: :ingredient_id
-  has many :recipes, through: :recipe_ingredients, foreign_key: :ingredient_id
+  has_many :user_recipes, foreign_key: :recipe_id
+  has_many :recipe_ingredients, foreign_key: :recipe_id
+  has many :users, through: :user_recipes, foreign_key: :user_id
+  has many :ingredients, through: :recipe_ingredients, foreign_key: :ingredient_id
 end
