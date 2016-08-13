363f8f797d45d49ddf74278695dbc4cf253ee8e5
macroscopic <mac.roche@gmail.com>
2016-08-12 17:09:44 -0700

    create controllers for user, home  and sessions: methods in sessions controller

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/profile'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'searches#index'
   resources :searches, only: [:new, :create, :index, :show]
