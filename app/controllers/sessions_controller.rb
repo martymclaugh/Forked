9d2f8022f1d89b4d2b350efa75b71f3c3c7b1ca4
Jason Sutter <jasonsutter87@gmail.com>
2016-08-14 14:40:45 -0700

    updated root url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     session[:omniauth] = auth.except('extra')
     user = User.sign_in_from_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url#, notice "SIGNED IN"
+    redirect_to 'recipes#home'#, notice "SIGNED IN"
   end
 
   def destroy
