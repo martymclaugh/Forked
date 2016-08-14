738dcb8b7c47edd8c69d5c3d10ca6964751e9f9a
Melanie Hahn <melaniesarah@gmail.com>
2016-08-14 15:17:22 -0700

    Add User routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,10 @@ Rails.application.routes.draw do
   get "/recipes/search_results" => 'recipes#search_results'
   get '/recipes/preview/:id' => 'recipes#preview'
 
-  get '/users/:id' => 'users#profile'
-
   get 'auth/:provider/callback', to: 'sessions#create'
   delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes
+  resources :users, only: [:show, :create]
 end
