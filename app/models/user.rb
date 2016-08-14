5660e792ad05af35821d1c823b2b5311af8db007
macroscopic <mac.roche@gmail.com>
2016-08-13 19:16:43 -0700

    fix typo on new migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
       provider: auth['provider'],
       uid: auth['uid'],
       name: auth['info']['name'],
-      image_url: auth['info']['image']
+      img_url: auth['info']['image']
     )
   end
 
