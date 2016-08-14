c8be2780eacda125237bb8b539663e08a3d33bbb
macroscopic <mac.roche@gmail.com>
2016-08-13 17:05:28 -0700

    fixes typo on userscontroller ln:8

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
   has_many :recipes, through: :user_recipes, foreign_key: :recipe_id
 
   def self.sign_in_from_omniauth(auth)
-    find_by(provider:auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+    find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
   end
 
-  def self.create_user_from_omni(auth)
+  def self.create_user_from_omniauth(auth)
     create(
       provider: auth['provider'],
       uid: auth['uid'],
