62ce3963433acab6050a8102752d55ec22c64542
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-14 13:54:36 -0700

    fix search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,12 @@ Rails.application.routes.draw do
   # root 'searches#index'
 
   get '/' => 'recipes#home'
-  # get 'home/index'
-
-  # get 'home/profile'
-
+  get '/recipes/search' => 'recipes#search'
+  get "/recipes/search_results" => 'recipes#search_results'
   get 'auth/:provider/callback', to: 'sessions#create'
 
   delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :searches, only: [:new, :create, :index, :show]
   resources :recipes
 end
