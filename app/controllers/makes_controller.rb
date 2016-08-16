8bb3a1ea5c093f7d186d088532d7d25ef0b1c64f
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-16 16:47:03 -0700

    update create route in makes controller to create a make, find or create a recipe, redirect back to that recipe

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -2,5 +2,6 @@ class MakesController < ApplicationController
   def create
     @recipe = Recipe.find_or_create_by(spoon_id: params['ingredients']['id'], title: params['ingredients']["title"], image: params['ingredients']['image'])
     @make = Make.create(user_id: current_user.id, recipe_id: @recipe.id)
+    redirect_to "/recipes/preview/#{@recipe.spoon_id}"
   end
 end
