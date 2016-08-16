7c2714def5c4fa974928effaf961b13375a483b0
Melanie Hahn <melaniesarah@gmail.com>
2016-08-16 10:26:13 -0700

    Add index action to Friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
 
-
-
+  def index
+    if params[:search]
+      @friends = User.search(params[:search])
+      if @friends.length > 0
+        @friends
+      else
+        @errors = "No users matched your search criteria"
+      end
+    end
+  end
 end
