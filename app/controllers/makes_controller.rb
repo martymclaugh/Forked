ce181e4df7095b64138b331b0a84dd4a5642321c
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-16 16:41:09 -0700

    define create method in makes controller

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -1,2 +1,6 @@
 class MakesController < ApplicationController
+  def create
+    @recipe = Recipe.find_or_create_by(spoon_id: params['ingredients']['id'], title: params['ingredients']["title"], image: params['ingredients']['image'])
+    @make = Make.create(user_id: current_user.id, recipe_id: @recipe.id)
+  end
 end
