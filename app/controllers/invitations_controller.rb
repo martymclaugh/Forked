39b0051f02fe25aed37ee8410b4577fd4b9ec034
Melanie Hahn <melaniesarah@gmail.com>
2016-08-17 12:35:23 -0700

    Add ability to create an invitation object

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,7 +6,8 @@ class InvitationsController < ApplicationController
   def create
     p params
     # Create new invitation object
-    invitation = Invitation.create()
+    invitation = Invitation.create(cuisine: params[:invitation][:cuisine], datetime: params[:invitation][:datetime], location: params[:invitation][:location])
+    p invitation
     # Create 2 new user_invitation objects
   end
 end
