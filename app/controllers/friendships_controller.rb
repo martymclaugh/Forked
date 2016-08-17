ace1bbd6d880c526f8370d77a98d0c57c8fb58db
macroscopic <mac.roche@gmail.com>
2016-08-16 18:14:05 -0700

    changes friendship index to display friend status of each searched user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,6 +4,7 @@ class FriendshipsController < ApplicationController
     if params[:search]
       @friends = User.search(params[:search])
       if @friends.length > 0
+        @friends.reject{|f| f.id == current_user.id}
         @friends
       else
         @errors = "No users matched your search criteria"
@@ -12,9 +13,21 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    p params
-    Friendship.new(user_id: current_user.id, friend_id: params(:id))
-    redirect :'friendships#index'
+    friend = Friendship.create(user_id: current_user.id, friend_id: params[:id])
+    friend2 = Friendship.create(user_id: params[:id], friend_id: current_user.id)
+     if friend.valid? && friend2.valid?
+       redirect_to "/users/#{params[:id]}"
+     else
+       redirect_to "/users/#{params[:id]}"
+     end
+  end
+
+  def destroy
+    friend = Friendship.find_by(user_id: current_user.id, friend_id: params[:id])
+    friend2 = Friendship.find_by(user_id: params[:id], friend_id: current_user.id)
+    Friendship.destroy(friend.id)
+    Friendship.destroy(friend2.id)
+    redirect_to "/users/#{params[:id]}"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes, :ingredients, :users
 
-  resources :friendships, only: [:index]
+  resources :friendships, only: [:index, :create, :destroy]
 end
