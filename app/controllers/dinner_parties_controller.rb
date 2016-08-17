fd656aab385d5be152b999d1f37a30b013635b85
Melanie Hahn <melaniesarah@gmail.com>
2016-08-17 12:27:32 -0700

    Add the ability to create a dinner party invitation from friend's user page. Invitation form is a modal

--- app/controllers/dinner_parties_controller.rb
+++ app/controllers/dinner_parties_controller.rb
@@ -1,2 +1,5 @@
 class DinnerPartiesController < ApplicationController
+
+  def create
+  end
 end
--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,12 @@
 class InvitationsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    p params
+    # Create new invitation object
+    invitation = Invitation.create()
+    # Create 2 new user_invitation objects
+  end
 end
